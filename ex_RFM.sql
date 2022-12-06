94df36e74b8b82d39e00dc2cc09c4c7057de0180
hieudataa <104623145+minhhieu247@users.noreply.github.com>
2022-12-06 22:21:43 +0700

    Add files via upload

---
+++ ex_RFM.sql
@@ -0,0 +1,18 @@
+﻿-- Lọc data
+with sumary as
+(
+	select CustomerID, OrderDate, sum(OrderTotal) as sumtotal
+	from Orders
+	group by  CustomerID, OrderDate
+	
+)
+-- calculate RFM(recency_frequence_Monetary Value)
+select CustomerID,
+datediff(day,max(OrderDate),GETDATE()),
+count(*),
+sum(sumtotal) ,
+ntile(6) over(order by datediff(day,max(OrderDate),GETDATE()) ) as R ,
+ntile (6) over (order by count(*)) as F,
+ntile(6) over (order by sum(sumtotal) ) as M
+from sumary
+group by CustomerID
